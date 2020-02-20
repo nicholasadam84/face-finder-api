aaf04fbaf8f0074ccebd8fa178a0540606a52b51
nicholasadam84 <nicholasadam84@gmail.com>
2020-02-20 15:51:04 -0800

    troubleshooting, added res.header lines in server.js

--- server.js
+++ server.js
@@ -36,9 +36,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
   });
 
 //Routes with Dependency Injection
